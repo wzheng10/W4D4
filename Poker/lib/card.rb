27119106cd415a17fa3f6c886feb9417e228f12f
akerpelm <akerpelm@u.rochester.edu>
2021-03-18 16:31:07 -0400

    Create TDD dir, begin Poker project

---
+++ Poker/lib/card.rb
@@ -0,0 +1,5 @@
+class Card
+
+    CARD_SUIT = [['diamonds'],['clovers'],['hearts'],['spades']]
+    CARD_VALUES = [[2],[3],[4],[5],[6],[7],[8],[9],[10],[J],[Q],[K],[A]]
+end
---
+++ Poker/spec/card_spec.rb
@@ -0,0 +1,11 @@
+require 'card.rb'
+
+describe Card do 
+
+    describe "#initialize" do
+    
+    
+
+    end
+
+end
