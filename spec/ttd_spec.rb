33bb10b52e940ecac090b7290520d7f9409a5509
akerpelm <akerpelm@u.rochester.edu>
2021-03-18 15:18:31 -0400

    Begin my_transpose spec

--- spec/ttd_spec.rb
+++ spec/ttd_spec.rb
@@ -13,16 +13,32 @@ describe Array do
     end
 
     describe "#two_sum" do
-    #use let(:var) {}
         arr = Array.new([-1,0,2,-2,1])
         it "returns pairs of positions sum = 0" do
             expect(arr.two_sum).to eq([[0,4],[2,3]])
         end
-        it "should be in order" do
-            var = arr.two_sum
-            expect(value[0][0]).to eq([[0,4],[2,3]])
-        end
 
+        it "should return an ordered array" do
+            var = arr.two_sum.sort
+            expect(arr.two_sum).to eq(var)
+        end
     end
 
+    context "#my_transpose" do
+        rows = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+                ]
+        cols = [
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]
+                ]
+
+        it "returns row in col" do
+            expect(rows.my_transpose).to eq(cols)
+        end
+        it "should not use built-in .transpose"
+    end
 end
