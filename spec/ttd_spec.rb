9a8bd83426f15bd9387c3e46f563a05a13b3685b
akerpelm <akerpelm@u.rochester.edu>
2021-03-18 14:59:03 -0400

    Test2

--- spec/ttd_spec.rb
+++ spec/ttd_spec.rb
@@ -7,19 +7,20 @@ describe Array do
 
     describe '#uniq' do
         it 'return all unique elements in an array' do
-                arr =  Array.new([1, 2, 1, 3, 3])
-                expect(arr.my_uniq).to eq([1, 2, 3]) 
+            arr =  Array.new([1, 2, 1, 3, 3])
+            expect(arr.my_uniq).to eq([1, 2, 3]) 
         end
     end
 
     describe "#two_sum" do
+    #use let(:var) {}
         arr = Array.new([-1,0,2,-2,1])
-        it 'should return an array of positions' do
-           expect(arr.two_sum).to eq([])
-        end
         it "returns pairs of positions sum = 0" do
-           
-            expect(arr.two_sum).to match_array([[0,4],[2,3]])
+            expect(arr.two_sum).to eq([[0,4],[2,3]])
+        end
+        it "should be in order" do
+            var = arr.two_sum
+            expect(value[0][0]).to eq([[0,4],[2,3]])
         end
 
     end
